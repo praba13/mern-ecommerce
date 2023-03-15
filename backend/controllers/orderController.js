82c127de5619416e59f1c24740adc7b6edfe2c67
Praba <prama1303@gmail.com>
2023-03-16 00:55:11 +0100

    Order Model & some Controllers added

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,60 @@
+const catchAsyncError = require('../middlewares/catchAsyncError');
+const Order = require('../models/orderModel');
+
+//Create New Order - api/v1/order/new
+exports.newOrder = catchAsyncError(async (req, res, next) => {
+  const {
+    orderItems,
+    shippingInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paymentInfo
+  } = req.body;
+
+  const order = await Order.create({
+    orderItems,
+    shippingInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paymentInfo,
+    paidAt: Date.now(),
+    user: req.user.id
+  });
+
+  res.status(200).json({
+    success: true,
+    order
+  });
+});
+
+//Get Single Order - api/v1/order/:id
+exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+  if (!order) {
+    return next(
+      new ErrorHandler(`Order not found with this id: ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    order
+  });
+});
+
+//Get Loggedin User Orders - /api/v1/myorders
+exports.myOrders = catchAsyncError(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user.id });
+
+  res.status(200).json({
+    success: true,
+    orders
+  });
+});
---
+++ backend/routes/order.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const { isAuthenticatedUser } = require('../middlewares/authenticate');
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders
+} = require('../controllers/orderController');
+
+router.route('/order/new').post(isAuthenticatedUser, newOrder);
+router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
+router.route('/myorders').get(isAuthenticatedUser, myOrders);
+
+module.exports = router;
