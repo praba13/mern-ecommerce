3624ddbeada62bde846c2b157f5b8611b718bf1d
Praba <prama1303@gmail.com>
2023-03-16 12:34:11 +0100

    Admin order controller added

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const { isAuthenticatedUser } = require('../middlewares/authenticate');
+const {
+  isAuthenticatedUser,
+  authorizeRoles
+} = require('../middlewares/authenticate');
 const {
   newOrder,
   getSingleOrder,
-  myOrders
+  myOrders,
+  orders,
+  updateOrder,
+  deleteOrder
 } = require('../controllers/orderController');
 
 router.route('/order/new').post(isAuthenticatedUser, newOrder);
 router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
 router.route('/myorders').get(isAuthenticatedUser, myOrders);
+
+//Admin Routes
+router
+  .route('/admin/orders')
+  .get(isAuthenticatedUser, authorizeRoles('admin'), orders);
+
+router
+  .route('/admin/order/:id')
+  .put(isAuthenticatedUser, authorizeRoles('admin'), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteOrder);
 
 module.exports = router;
